a1bbeed42585a5772e134998c6902de0f5e2ebb0
wang <nfunqn@gmail.com>
2022-05-28 09:22:35 +0800

    窗口

--- windows.py
+++ windows.py
@@ -1,68 +1,64 @@
-from PIL import Image, ImageTk # 导入图像处理函数库
-import tkinter as tk
-from tkinter import filedialog   #导入文件对话框函数库
+from tkinter import *
+from tkinter import  filedialog
 import gender_test as gt
 from torch.autograd import Variable
 import torch
+from PIL import Image, ImageTk
+
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.place()
+        self.createWidget()
+
+    def createWidget(self):
+        global photo
+        photo = None
+        global wenben
+        wenben = None
+        self.label03 = Label(self, image=photo)
+        self.label03.grid(column=0, row=0)
+        # self.label03.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.label04 = Label(self, text=wenben)
+        self.label04.grid(column=0, row=0)
+
+        self.btn01 = Button(self, text='打开', command=self.getfile, bg='white', anchor='s')
+        self.btn01.grid(column=0, row=1)
+        # self.sbtn01.place(relx=0.8, rely=0.5, anchor=CENTER)
+
+    def getfile(self):
+        file_path = filedialog.askopenfilename(title='选择文件', filetypes=[(('JPG', '*.jpg')), ('All Files', '*')])
+        img = Image.open(file_path)
+        width, height = img.size
+
+        img = img.resize((700, int(700 / width * height)))
+
+        net = gt.reload_net()
+        image = gt.image_loader(file_path)
+
+        outputs = net(Variable(image))
+        classes = ('男', '女')
+        _, predicted = torch.max(outputs.data, 1)
+        sex = classes[predicted]
 
 
 
-# 建立窗口 设定大小并命名
-window = tk.Tk()
-window.title('图像显示界面')
-window.geometry('600x500')
-global img_png           # 定义全局变量 图像的
-var = tk.StringVar()    # 这时文字变量储存器
-global img_path
-# 建立打开图像和显示图像函数
+        global photo
+        global wenben
+        wenben = str(sex)
+        photo = ImageTk.PhotoImage(img)  # 实际上是把这个变成全局变量
+        self.label03.configure(image=photo)
+        self.label03.image = photo
+        self.label04.configure(text=wenben)
 
 
-def Open_Img():
-    global img_path
-    global img_png
-    OpenFile = tk.Tk() #建立新窗口
-    OpenFile.withdraw()
-    file_path = filedialog.askopenfilename()
-    img_path = file_path
-    Img = Image.open(file_path)
-    img_png = ImageTk.PhotoImage(Img)
-    var.set('已打开')
-def Show_Img():
-    global img_png
-    global img_path
-    print(type(img_path))
 
-    net = gt.reload_net()
-    image = gt.image_loader(img_path)
 
-    outputs = net(Variable(image))
-    classes = ('男', '女')
-    _, predicted = torch.max(outputs.data, 1)
 
-    print(classes[predicted])
+root = Tk()
+root.geometry('700x680')
+app = Application(master=root)
 
-    var.set('已显示')   # 设置标签的文字为 'you hit me'
-    label_Img = tk.Label(window, image=img_png)
-
-    label_Img.pack()
-
-# 建立文本窗口，显示当前操做状态
-Label_Show = tk.Label(window,
-    textvariable=var,   # 使用 textvariable 替换 text, 由于这个能够变化
-    bg='blue', font=('Arial', 12), width=15, height=2)
-Label_Show.pack()
-# 建立打开图像按钮
-btn_Open = tk.Button(window,
-    text='打开图像',      # 显示在按钮上的文字
-    width=15, height=2,
-    command=Open_Img)     # 点击按钮式执行的命令
-btn_Open.pack()    # 按钮位置
-# 建立显示图像按钮
-btn_Show = tk.Button(window,
-    text='显示图像',      # 显示在按钮上的文字
-    width=15, height=2,
-    command=Show_Img)     # 点击按钮式执行的命令
-btn_Show.pack()    # 按钮位置
-
-# 运行总体窗口
-window.mainloop()
+root.mainloop()
